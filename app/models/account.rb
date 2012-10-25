6fb97a4ddff2890244bd076c646497535ea6781e
Nicolas Viennot <nicolas@viennot.biz>
2012-10-25 15:43:09 -0400

    Two different tokens for secure and insecure mode

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,16 +4,21 @@ class Account
   field :email
   field :password
   field :android_id
-  field :auth_token
+  field :auth_token_plain
+  field :auth_token_secure
 
   def session(options={})
-    @session ||= Market::Session.new(options[:secure] || false).tap do |s|
-      if auth_token.present?
+    secure = options[:secure] || false
+    auth_token_name = secure ? :auth_token_secure : :auth_token_plain
+
+    @session ||= Market::Session.new(secure).tap do |s|
+      token = __send__(auth_token_name)
+      if token.present?
         s.setAndroidId(self.android_id)
-        s.setAuthSubToken(auth_token)
+        s.setAuthSubToken(token)
       else
         s.login(email, password, android_id)
-        update_attributes(:auth_token => s.authSubToken)
+        update_attributes(auth_token_name => s.authSubToken)
       end
     end
   end
