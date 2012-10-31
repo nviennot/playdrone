4b903385bebd3ef50eca23a97cf5c6fa22bfef76
Nicolas Viennot <nicolas@viennot.biz>
2012-10-30 21:19:10 -0400

    Adding accounts

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,21 @@
+class AccountsController < ApplicationController
+  respond_to :html
+
+  def new
+    @account = Account.new
+    respond_with @account
+  end
+
+  def create
+    @account = Account.new(params[:account])
+
+    begin
+      @account.checkin!
+      flash[:notice] = "Thank you :)"
+    rescue
+      flash[:error] = "Hum... The login information seems incorrect :("
+    end
+
+    respond_with @account, :location => :new_accounts
+  end
+end
