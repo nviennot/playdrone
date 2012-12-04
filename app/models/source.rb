f858f17f078edf1ad8b9484e13c464366370c425
Nicolas Viennot <nicolas@viennot.biz>
2012-12-04 18:37:19 -0500

    find_path gives us a breakdown of used libraries

--- app/models/source.rb
+++ app/models/source.rb
@@ -41,6 +41,18 @@ class Source
     Tire::Configuration.client.delete "#{index.url}/_query?q=apk_eid:#{apk.eid}"
   end
 
+  def self.find_path(query)
+    res = tire.search(:per_page => 0) do
+      query { @value = { :wildcard => { :path => query } } }
+      facet(:path) { terms :field => :path, :size => 100 }
+    end
+
+    {
+      :total  => res.total,
+      :detail => Hash[res.facets['path']['terms'].map { |f| [f['term'], f['count']] }]
+    }
+  end
+
   def self.search(query, options={})
     tire.search(options) do
       query     { string query, :default_field => :lines, :default_operator => 'AND' }
