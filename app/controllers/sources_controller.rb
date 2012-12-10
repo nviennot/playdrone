6a60d7d249f4959cdb58dd890c39f5ad06304add
Nicolas Viennot <nicolas@viennot.biz>
2012-12-10 02:37:01 -0500

    Filter out libs for searches

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -18,7 +18,13 @@ class SourcesController < ApplicationController
     return unless user_query = params[:query]
 
     @results = Source.tire.search :page => params[:page].to_i, :per_page => (params[:per_page] || 10).to_i do
-      query { string user_query, :default_field => :lines, :default_operator => 'AND' }
+      query do
+        filtered do
+          query { string user_query, :default_field => :lines, :default_operator => 'AND' }
+          filter :not, :exists => {:field => :lib}
+        end
+      end
+
       highlight :lines => { :fragment_size => 300, :number_of_fragments => 100000 }, :options => { :tag => '' }
       fields :apk_eid, :path
 
