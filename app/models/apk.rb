9e7384778cdb460522ffbaa669075b0a4847f9d4
Phil G. Sphicas <pgs2111@columbia.edu>
2013-02-13 00:32:57 -0800

    Elastic search for apks

--- app/models/apk.rb
+++ app/models/apk.rb
@@ -1,6 +1,8 @@
 class Apk
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
 
   field :package_name
   field :version_code
@@ -77,5 +79,46 @@ class Apk
 
   def source_dir
     Rails.root.join('play', 'src', file.basename)
+  end
+
+  INDEXED_FIELDS = %w(eid title app_type category creator contact_email
+  contact_phone contact_website version downloads rating rating_count
+  description promo_text screenshots_count recent_changes promo_video price
+  price_currency permissions asset_size released_at lib_names secured)
+
+  tire.mapping :_all => {:enabled => false} do
+    indexes :eid,               :index    => :not_analyzed, :store => :yes
+    indexes :title,             :analyzer => :simple
+    indexes :app_type,          :index    => :not_analyzed
+    indexes :category,          :index    => :not_analyzed
+    indexes :creator,           :index    => :not_analyzed
+    indexes :contact_email,     :index    => :not_analyzed
+    indexes :contact_phone,     :index    => :not_analyzed
+    indexes :contact_website,   :index    => :not_analyzed
+    indexes :version,           :index    => :not_analyzed
+    indexes :downloads,         :type     => :integer, :store => :yes
+    indexes :rating,            :type     => :float
+    indexes :rating_count,      :type     => :integer
+    indexes :description,       :analyzer => :simple
+    indexes :promo_text,        :analyzer => :simple
+    indexes :screenshots_count, :type     => :integer
+    indexes :recent_changes,    :analyzer => :simple
+    indexes :promo_video,       :index    => :not_analyzed
+    indexes :price,             :type     => :float
+    indexes :price_currency,    :index    => :not_analyzed
+    indexes :permissions,       :analyzer => :keyword
+    indexes :asset_size,        :type     => :integer
+    indexes :released_at,       :type     => :date
+    indexes :lib_names,         :analyzer => :keyword
+    indexes :secured,           :type     => :boolean
+  end
+
+  def self.paginate(options)
+    self.page(options[:page])
+  end
+
+  def to_indexed_json
+    # this is the payload for elasticsearch
+    self.app.attributes.merge(self.attributes).select { |k,v| k.in? INDEXED_FIELDS }.to_json
   end
 end
