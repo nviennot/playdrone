2cf2ba78b84eefee1b0fd0443b3bbbc0c3e642e5
Nicolas Viennot <nicolas@viennot.biz>
2013-04-16 02:47:26 -0400

    API hooks pretty much done

--- lib/market.rb
+++ lib/market.rb
@@ -1,43 +1,24 @@
 require "#{Rails.root}/vendor/googleplay.pb"
 
 module Market
-  def self.connection
-    # Note that first_usable calls rate_limit!
-    account = Account.first_usable
-
+  def self.api
+    # Lazy load because it's a huge file
     Faraday.new(:url => 'https://android.clients.google.com/fdfe/') do |faraday|
-      faraday.request  :url_encoded
-      faraday.response :logger
-      faraday.adapter  Faraday.default_adapter
-      faraday.headers = {
-        "Accept-Language"               => 'en',
-        "Authorization"                 => "GoogleLogin auth=#{account.auth_token}",
-        "X-DFE-Enabled-Experiments"     => "cl:billing.select_add_instrument_by_default",
-        "X-DFE-Unsupported-Experiments" => "nocache:billing.use_charging_poller,market_emails,buyer_currency,prod_baseline,checkin.set_asset_paid_app_field,shekel_test,content_ratings,buyer_currency_in_app,nocache:encrypted_apk,recent_changes",
-        "X-DFE-Device-Id"               => account.android_id,
-        "X-DFE-Client-Id"               => "am-android-google",
-        #"X-DFE-Logging-Id"             => self.loggingId2, # Deprecated?
-        "User-Agent"                    => "Android-Finsky/3.7.13 (api=3,versionCode=8013013,sdk=16,device=crespo,hardware=herring,product=soju)",
-        "X-DFE-SmallestScreenWidthDp"   => "320",
-        "X-DFE-Filter-Level"            => "3",
-        "Accept-Encoding"               => "",
-        "Host"                          => "android.clients.google.com"
-      }
+      faraday.use     Market::Middleware
+      faraday.adapter Faraday.default_adapter
     end
   end
-
-  ### Search ###
 
   PER_PAGE = 20
   MAX_START = 500
 
   class SearchResult < Struct.new(:payload)
     def num_apps
-      payload[:container_metadata][:estimated_results]
+      payload[:payload][:search_response][:doc][0][:container_metadata][:estimated_results]
     end
 
     def app_ids
-      payload[:child].map { |app| app[:docid] }
+      payload[:payload][:search_response][:doc][0][:child].map { |app| app[:docid] }
     end
   end
 
@@ -47,9 +28,17 @@ module Market
     params[:q] = query
     params[:n] = options[:per_page] if options[:per_page]
     params[:o] = options[:start]    if options[:start]
-    r = connection.get('search', params)
-    # etag = r.env[:response_headers]["etag"] # The etag is useless :(
-    r = ::GooglePlay::ResponseWrapper.new.parse_from_string(r.body).to_hash
-    SearchResult.new(r[:payload][:search_response][:doc][0])
+    SearchResult.new api.get('search', params).body
+  end
+
+  def self.app_details_bulk(app_ids)
+    request = ::GooglePlay::BulkDetailsRequest.new
+    request.doc_id = app_ids
+    request.include_child_docs = true
+    api.post('bulkDetails', request).body
+  end
+
+  def self.app_details(app_id)
+    api.get('details', :doc => app_id).body
   end
 end
---
+++ lib/market/middleware.rb
@@ -0,0 +1,47 @@
+module Market
+  class TooManyRequests < RuntimeError; end
+  class BadRequest < RuntimeError; end
+
+  class Middleware < Faraday::Response::Middleware
+    def call(env)
+      # Note that first_usable calls rate_limit!
+      account = Account.first_usable
+      env[:account] = account
+
+      env[:request_headers].merge!(
+        "Accept-Language"               => 'en',
+        "Authorization"                 => "GoogleLogin auth=#{account.auth_token}",
+        "X-DFE-Enabled-Experiments"     => "cl:billing.select_add_instrument_by_default",
+        "X-DFE-Unsupported-Experiments" => "nocache:billing.use_charging_poller,market_emails,buyer_currency,prod_baseline,checkin.set_asset_paid_app_field,shekel_test,content_ratings,buyer_currency_in_app,nocache:encrypted_apk,recent_changes",
+        "X-DFE-Device-Id"               => account.android_id,
+        "X-DFE-Client-Id"               => "am-android-google",
+        #"X-DFE-Logging-Id"             => self.loggingId2, # Deprecated?
+        "User-Agent"                    => "Android-Finsky/3.7.13 (api=3,versionCode=8013013,sdk=16,device=crespo,hardware=herring,product=soju)",
+        "X-DFE-SmallestScreenWidthDp"   => "320",
+        "X-DFE-Filter-Level"            => "3",
+        "Accept-Encoding"               => "",
+        "Host"                          => "android.clients.google.com"
+      )
+
+      if env[:body].is_a? ::Protobuf::Message
+        env[:body] = env[:body].serialize
+        env[:request_headers]['Content-Type'] = 'application/x-protobuf'
+      end
+
+      super
+    end
+
+    def on_complete(env)
+      if env[:status] == 429
+        env[:account].disable!
+        raise Market::TooManyRequests
+      end
+
+      env[:body] = ::GooglePlay::ResponseWrapper.new.parse_from_string(env[:body]).to_hash
+
+      unless env[:status] == 200
+        raise Market::BadRequest.new :status => env[:status], :body => env[:body]
+      end
+    end
+  end
+end
