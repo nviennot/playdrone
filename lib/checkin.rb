0e63d0e37458aa4a61306625b1fae613750bc182
Nicolas Viennot <nicolas@viennot.biz>
2013-04-15 20:12:53 -0400

    Login

--- lib/checkin.rb
+++
@@ -1,7 +0,0 @@
-module Checkin
-  require Rails.root.join 'vendor', 'android-checkin-1.0.jar'
-
-  def self.checkin(email, password)
-    Java::ComAndroidCheckin::Checkin.new(email, password).checkin
-  end
-end
