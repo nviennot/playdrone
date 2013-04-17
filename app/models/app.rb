271bf56130f8e86115645153f3b073b40685a053
Nicolas Viennot <nicolas@viennot.biz>
2013-04-17 04:27:52 -0400

    Base App mapping in place

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,69 +1,43 @@
 class App
-  include Mongoid::Document
-  include Sidekiq::Worker
-  include Mongoid::Timestamps
+  mapping({
+    :id                => { :type => :string, :index    => :not_analyzed },
+    :title             => { :type => :string, :analyzer => :simple },
+    :description       => { :type => :string, :analyzer => :simple },       # html
+    :recent_changes    => { :type => :string, :index    => :not_analyzed }, # html
+    :creator           => { :type => :string, :index    => :not_analyzed },
+    :developer_email   => { :type => :string, :index    => :not_analyzed },
+    :developer_website => { :type => :string, :index    => :not_analyzed },
+    :top_developer     => { :type => :boolean },
+    :editors_choice    => { :type => :boolean },
+    :content_rating    => { :type => :integer },
+    :app_type          => { :type => :string, :index    => :not_analyzed },
 
-  belongs_to :searched_category, :class_name => 'Category', :foreign_key => :category_id
+    :price    => { :type => :float }, # always in USD
+    :currency => { :type => :string, :index => :not_analyzed },
 
-  field :app_id
-  field :title
-  field :app_type,     :type => Symbol
-  field :creator
-  field :version
-  field :rating,       :type => Float
-  field :rating_count, :type => Integer
-  field :description
-  field :permissions,  :type => Array
-  field :install_size, :type => Integer
-  field :category
-  field :contact_email
-  field :downloads_count_text
-  field :downloads, :type => Integer, :default => ->{ _downloads }
-  field :contact_phone
-  field :contact_website
-  field :screenshots_count, :type => Integer
-  field :promo_text
-  field :recent_changes
-  field :promo_video
+    :available_if_owned => { :type => :boolean }, # what is this? I guess we'll find out
 
-  field :_id, :as => :package_name
-  field :version_code
+    :version_code      => { :type => :integer },
+    :version_string    => { :type => :string, :index => :no },
+    :installation_size => { :type => :integer },
+    :permission        => { :type => :string, :index => :not_analyzed },
 
-  field :price_currency
-  field :price
+    :crawl_date  => { :type => :date, :store => true },
+    :upload_date => { :type => :date, :store => true },
 
-  index :app_id => 1
-  index :price => 1
-  index :downloads_count_text => 1
-  index :downloads => 1
-  index :price => 1, :downloads_count_text => 1
+    :downloads     => { :type => :integer, :store => true },
+    :star_rating   => { :type => :float },
+    :ratings_count => { :type => :integer },
+    :one_star      => { :type => :integer },
+    :two_star      => { :type => :integer },
+    :three_star    => { :type => :integer },
+    :four_star     => { :type => :integer },
+    :five_star     => { :type => :integer },
+    :comment_count => { :type => :integer },
 
-  has_many :apks, :foreign_key => :package_name
-
-  def _downloads
-    case downloads_count_text.gsub(/,/,"")
-    when /</         then 0
-    when /(.*)-(.*)/ then $1
-    when />(.*)/     then $1
-    end.to_i
-  end
-
-  def download_latest_apk!(options={})
-    force = options[:force]
-
-    raise "This is a paied app" if price
-
-    apk = apks.first # for now, we don't care about updates
-    #apk = apks.where(:version_code => version_code).first
-    if apk
-      # Racy, but okay because this will be fired manually
-      apk.download! if force
-    else
-      # Racy, but okay because of unique index
-      apk = apks.create!(:version_code => version_code,
-                         :version      => version,
-                         :asset_id     => app_id)
-      apk.download!
-    end
-  end
+    :image => { :properties => {
+      :type => { :type => :integer },
+      :url  => { :type => :string, :index => :no },
+    }},
+  })
 end
