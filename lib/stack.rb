951a2325e7f69f48566335a0fc6e76eee0ee8402
Nicolas Viennot <nicolas@viennot.biz>
2013-04-20 16:18:50 -0400

    Per day crawling

--- lib/stack.rb
+++ lib/stack.rb
@@ -2,14 +2,15 @@ module Stack
   # Each step in a stack must be idempotent, so the chain can fail at any point,
   # and we can retry the whole thing.
 
-  def self.process_app(app_id)
+  def self.process_app(app_id, crawled_at)
     @create_app_stack ||= ::Middleware::Builder.new do
       use LockApp
       use PrepareFS
       use FetchMarketDetails
-      use DownloadApk
-      use DecompileApk
+      use UpdateIndex
+        use DownloadApk
+        use DecompileApk
     end
-    @create_app_stack.call :app_id => app_id, :crawl_date => Date.today
+    @create_app_stack.call :app_id => app_id, :crawled_at => crawled_at
   end
 end
