4cc636e0e7884ef16c5ff6611f05cdd934e7a589
Nicolas Viennot <nicolas@viennot.biz>
2013-04-20 19:45:25 -0400

    UpdateIndex -> IndexApp

--- lib/stack/lock_app.rb
+++ lib/stack/lock_app.rb
@@ -1,10 +1,7 @@
 class Stack::LockApp < Stack::Base
   def call(env)
-    mutex = Redis::Lock.new(env[:app_id], :timeout => 10.seconds,
-                                          :sleep   => 1.second,
-                                          :expire  => 5.minutes,
-                                          :node    => Redis.instance)
-
+    options = { :timeout => 10.seconds, :sleep => 1.second, :expire => 5.minutes, :node => Redis.instance }
+    mutex = Redis::Lock.new(env[:app_id], options)
     mutex.lock
     @stack.call(env)
   ensure
