0ca56e0baa5f158ef13ec18593312c69f56a7b21
Nicolas Viennot <nicolas@viennot.biz>
2013-04-21 01:42:39 -0400

    Search is back

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,39 +1,51 @@
+require 'will_paginate/collection'
+
 class SourcesController < ApplicationController
-  def show
-    @apk_eid  = apk_eid  = params[:apk_eid]
-    @apk_path = apk_path = params[:apk_path]
-
-    @results = Source.tire.search :size => 1 do
-      query do
-        boolean do
-          must { term :apk_eid,  apk_eid  }
-          must { term :path,     apk_path }
-        end
-      end
-      fields :lines
-    end
-  end
-
   def search
-    return unless user_query = params[:query]
+    user_query = params[:query]
+    per_page   = (params[:per_page] || 10).to_i
+    page       = (params[:page] || 1).to_i
+    from       = (page-1) * per_page
+    regex      = Regexp.new(params[:filter], Regexp::IGNORECASE) if params[:filter].present?
+    return unless user_query
 
-    @results = Source.tire.search :page => params[:page].to_i, :per_page => (params[:per_page] || 10).to_i do
-      query do
-        filtered do
-          query { string user_query, :default_field => :lines, :default_operator => 'AND' }
-          #filter :not, :exists => {:field => :lib}
-          filter :term, :core => true
-        end
-      end
 
-      highlight :lines => { :fragment_size => 300, :number_of_fragments => 100000 }, :options => { :tag => '' }
-      fields :apk_eid, :path
+    @results = Source.index(:live).search({
+      :from => from,
+      :size => per_page,
 
-      facet(:num_lines) { statistical :num_lines }
-      facet(:size)      { statistical :size }
-    end
+      :query => {
+        :filtered => {
+          :query => {
+            :query_string => {
+              :query            => user_query,
+              :default_field    => :lines,
+              :default_operator => 'AND'
+            }
+          }
+        }
+      },
 
-    @regex = Regexp.new(params[:filter], Regexp::IGNORECASE) if params[:filter].present?
-    @files = Source.filter_lines(@results, @regex)
+      :fields => [:app_id, :filename],
+
+      :highlight => {
+        :pre_tags  => [''],
+        :post_tags => [''],
+        :fields    => { :lines => { :fragment_size => 300, :number_of_fragments => 100000 } }
+      }
+    })
+
+    @files = @results.results.map do |source|
+      next unless source._highlight
+      matched_lines = source._highlight.lines
+      matched_lines = matched_lines.grep(regex) if regex
+      next if matched_lines.empty?
+
+      {:app_id   => source.app_id,
+       :filename => source.filename,
+       :lines    => matched_lines}
+    end.compact
+
+    @pagination = WillPaginate::Collection.new(page, per_page, @results.total)
   end
 end
