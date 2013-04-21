4ea2198047d8a35927837ea76dc37648466a79ff
Nicolas Viennot <nicolas@viennot.biz>
2013-04-21 02:58:10 -0400

    Add git diff

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -3,6 +3,8 @@ class AppsController < ApplicationController
     @app_id = params[:app_id]
     @app = App.find(:live, @app_id)
 
+    @diff = `cd #{Repository.new(@app_id).path} && git log --color --stat src -- src | #{Rails.root.join('script/ansi2html.sh')}` rescue nil
+
     @results = Source.index(:live).search({
       :size   => 100000,
       :query  => { :term     => { :app_id => @app_id } },
