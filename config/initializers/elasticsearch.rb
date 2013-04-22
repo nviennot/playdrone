b3fc1ad35c0abed2ea9ca2868039d3318138e3bb
Nicolas Viennot <nicolas@viennot.biz>
2013-04-22 01:34:01 -0400

    Use local ES if available on production

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,3 @@
-ENV['ELASTICSEARCH_URL'] ||= YAML.load_file(Rails.root.join("config", "elasticsearch.yml"))[Rails.env]['urls'].sample
+es_url = 'http://localhost:9200' if Rails.env.production? && Dir.exists?('/usr/local/var/run/elasticsearch')
+es_url ||= YAML.load_file(Rails.root.join("config", "elasticsearch.yml"))[Rails.env]['url']
+ENV['ELASTICSEARCH_URL'] = es_url
