c682b6e7df0221fd9fc940060d9bace599a323c6
Nicolas Viennot <nicolas@viennot.biz>
2013-04-22 01:42:46 -0400

    Deploy settings for Vagrant

--- deploy/settings.rb
+++ deploy/settings.rb
@@ -1,4 +1,4 @@
-set :repository, 'git://github.com/nviennot/google-play-crawler.git'
+set :repository, 'git@github.com:nviennot/google-play-crawler.git'
 set :scm, :git
 set :branch, 'master'
 set :deploy_via, :remote_cache
@@ -6,14 +6,15 @@ set :scm_verbose, true
 
 set :user, 'root'
 set :use_sudo, false
+ssh_options[:forward_agent] = true
 
 set :application, 'google-play-crawler'
-set :rvm_ruby_string, 'jruby-1.7.0@google-play-crawler'
+set :rvm_ruby_string, 'ruby-1.9.3-p327@google-play-crawler'
 set :deploy_to, "/srv/#{application}"
 set :keep_releases, 3
 set :normalize_asset_timestamps, false
 set :rake, 'bundle exec rake'
 set :rvm_type, :system
 
-role :web, '<server_url>'
-role :app, '<server_url>'
+role :unicorn, '10.1.1.11'
+role :sidekiq, '10.1.1.11', '10.1.1.12'
