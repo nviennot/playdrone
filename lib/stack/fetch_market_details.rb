de3ef22ccbfc16afbda02b3ecdbc1f11fae62c50
Nicolas Viennot <nicolas@viennot.biz>
2013-04-24 04:40:19 -0400

    Dealing with app that get removed from the market

--- lib/stack/fetch_market_details.rb
+++ lib/stack/fetch_market_details.rb
@@ -2,55 +2,79 @@ class Stack::FetchMarketDetails < Stack::BaseGit
   use_git :branch => :market_metadata, :tag_with => :crawl_date
 
   def persist_to_git(env, git)
-    app_details = Market.details(env[:app_id])
-
-    if Rails.env.production? && !Dir.exists?('/home/vagrant')
-      App.discovered_apps(app_details.related_app_ids)
-    end
-
-    if env[:crawled_at] != Date.today &&
-       env[:crawled_at] != Date.today - 1.day
+    if env[:crawled_at] < Date.today - 1.day
       raise "Missed the time to crawl data. Crap."
     end
 
-    env[:raw_app] = app_details.raw_app
-    env[:app] = App.from_market(app_details.raw_app)
-    decorate_app(env)
+    begin
+      app_details = Market.details(env[:app_id])
 
-    git.commit do |index|
-      index.add_file('metadata.json', MultiJson.dump(env[:raw_app], :pretty => true))
+      if Rails.env.production? && !Dir.exists?('/home/vagrant')
+        App.discovered_apps(app_details.related_app_ids)
+      end
+
+      instantiate_app(env, app_details.raw_app)
+
+      git.commit do |index|
+        index.add_file('metadata.json', MultiJson.dump(app_details.raw_app, :pretty => true))
+      end
+
+      @stack.call(env)
+
+    rescue Market::NotFound => e
+      instantiate_app(env, nil)
+
+      git.commit do |index|
+        index.add_file('not_found', e.to_s)
+      end
+      # Chain halts
     end
-    @stack.call(env)
   end
 
   def parse_from_git(env, git)
-    env[:raw_app] = MultiJson.load(git.read_file('metadata.json'), :symbolize_keys => true)
-    env[:app] = App.from_market(env[:raw_app])
-    decorate_app(env)
-    @stack.call(env)
+    if git.read_file('not_found')
+      instantiate_app(env, nil)
+    else
+      instantiate_app(env, MultiJson.load(git.read_file('metadata.json'), :symbolize_keys => true))
+      @stack.call(env)
+      # Chain halts
+    end
   end
 
   private
 
-  def decorate_app(env)
+  def populate_yesterday_app(env)
+    yesterday_tag_ref = "refs/tags/market_metadata-#{env[:crawled_at] - 1.day}"
+    yesterday_commit_sha = Rugged::Reference.lookup(env[:repo], yesterday_tag_ref).try(:target)
+
+    return unless yesterday_commit_sha
+    yesterday_metadata = env[:repo].lookup(yesterday_commit_sha).tree['metadata.json']
+    return unless yesterday_metadata
+    yesterday_json = env[:repo].lookup(yesterday_metadata[:oid]).read_raw.data
+    env[:yesterday_app] = App.from_market(MultiJson.load(yesterday_json, :symbolize_keys => true))
+  end
+
+  def instantiate_app(env, raw_app)
+    if raw_app
+      env[:app] = App.from_market(raw_app)
+    else
+      env[:app] = App.new.tap { |app| app._id = env[:app_id] }
+      env[:app_not_found] = true
+    end
+
     env[:app].crawled_at = env[:crawled_at]
 
-    tag_ref = "refs/tags/market_metadata-#{env[:crawled_at] - 1.day}"
-    last_commit_sha = Rugged::Reference.lookup(env[:repo], tag_ref).try(:target)
-
-    if last_commit_sha
-      old_metadata = env[:repo].lookup(last_commit_sha).tree['metadata.json']
-      old_json = env[:repo].lookup(old_metadata[:oid]).read_raw.data
-      old_app = App.from_market(MultiJson.load(old_json, :symbolize_keys => true))
-      new_app = App.from_market(env[:raw_app])
-
+    populate_yesterday_app(env)
+    if env[:yesterday_app]
       env[:app].market_released = false
-      env[:app].apk_updated     = old_app.version_code != new_app.version_code
+      env[:app].apk_updated     = raw_app ? false : (env[:yesterday_app].version_code != env[:app].version_code)
+      env[:app].market_removed  = !!raw_app
     else
       # XXX If we don't have the data from the day before, we are going to make bad decisions
       # FIXME
       env[:app].market_released = true
       env[:app].apk_updated     = false
+      env[:app].market_removed  = false
     end
   end
 end
