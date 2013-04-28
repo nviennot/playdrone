f6925c1857e2754a1fa4f09c51d7ee1dd3a015d4
Nicolas Viennot <nicolas@viennot.biz>
2013-04-28 01:32:19 -0400

    cron with whenever

--- deploy/settings.rb
+++ deploy/settings.rb
@@ -16,10 +16,14 @@ set :normalize_asset_timestamps, false
 set :rake, 'bundle exec rake'
 set :rvm_type, :system
 
+set :whenever_roles, [:sidekiq, :master]
+set :whenever_command, 'bundle exec whenever'
+
 if ENV['VAGRANT']
   role :unicorn, '10.1.1.11'
   role :sidekiq, '10.1.1.11', '10.1.1.12'
   role :metrics, '10.1.1.12'
+  role :master,  '10.1.1.11'
 else
   role :unicorn, 'node01.googleplaywith.me', 'node02.googleplaywith.me',
                  'node03.googleplaywith.me', 'node04.googleplaywith.me',
@@ -34,4 +38,5 @@ else
                  'node09.googleplaywith.me', 'node10.googleplaywith.me'
 
   role :metrics, 'node01.googleplaywith.me'
+  role :master,  'node01.googleplaywith.me'
 end
