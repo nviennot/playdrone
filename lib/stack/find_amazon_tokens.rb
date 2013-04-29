f22d2f28f1ae5746c41c735ed39e69f970ef6778
Phil G. Sphicas <pgs2111@columbia.edu>
2013-04-29 01:02:59 -0700

    added amazon, facebook, and linkedin

--- lib/stack/find_amazon_tokens.rb
+++ lib/stack/find_amazon_tokens.rb
@@ -1,24 +1,5 @@
 class Stack::FindAmazonTokens < Stack::BaseTokenFinder
-
-  def find_tokens(env)
-    src_dir = env[:src_dir]
-    aws = { :key => [], :secret => [] }
-    lines = exec_and_capture('script/find_amazon_tokens',src_dir)
-    lines.each_line do |line|
-      Rails.logger.info line
-      if line =~ /"([^",() ]{40})"/
-        secret = $1
-        if secret =~ /[0-9]/ && secret =~ /([A-Z].*){4}/ && secret =~ /([a-z].*){4}/
-          aws[:secret] << secret unless secret == "3i2ndDfv2rTHiSisAbouNdArYfORhtTPEefj3q2f"
-        end
-      end
-
-      if line =~ /\b(AKIA[^"]{16})\b/
-        key = $1
-        aws[:key] << key
-      end
-    end
-    aws.values.map { |v| v.uniq! }
-    env[:amazon_tokens] = aws
-  end
+  tokens :amazon, :access_key_id     => '[="](AKIA[0-9A-Z]{16})[&"]',
+                  :secret_access_key => '[="]([0-9a-zA-Z\/\+]{40})[&"]',
+                  :proximity         => 5
 end
--- lib/stack/find_facebook_tokens.rb
+++ lib/stack/find_facebook_tokens.rb
@@ -1,23 +1,4 @@
 class Stack::FindFacebookTokens < Stack::BaseTokenFinder
-
-  def find_tokens(env)
-    src_dir = env[:src_dir]
-    facebook = { :app_id => [], :app_secret => [] }
-    lines = exec_and_capture('script/find_facebook_tokens',src_dir)
-    #Rails.logger.info lines
-    lines.each_line do |line|
-          if line =~ /"(\d{13,17})"/
-            app_id = $1
-            facebook[:app_id] << app_id
-          end
-
-          if line =~ /"([0-9a-f]{32})"/
-            app_secret = $1
-            facebook[:app_secret] << app_secret
-          end
-        end
-    facebook.values.map { |v| v.uniq! }
-    env[:facebook_tokens] = facebook
-  end
-
+  tokens :facebook, :app_secret => '[="]([0-9a-f]{32})[&"]',
+                    :app_id     => '[="]([0-9]{13,17})[&"]'
 end
