c255e166e3a19dcf2a4575f85c2293a6962cb769
Phil G. Sphicas <pgs2111@columbia.edu>
2013-04-30 22:55:06 -0700

    login must have lowercase

--- lib/stack/find_bitlyv1_tokens.rb
+++ lib/stack/find_bitlyv1_tokens.rb
@@ -1,4 +1,4 @@
 class Stack::FindBitlyv1Tokens < Stack::BaseTokenFinder
   tokens :bitlyv1, :api_key           => '[="](R_[0-9a-f]{32})[&"]',
-                   :login             => '[="]([0-9a-zA-Z_]{5,31})[&"]'
+                   :login             => { :matcher => '[="]([0-9a-zA-Z_]{5,31})[&"]', :must_have => /[a-z]/ }
 end
