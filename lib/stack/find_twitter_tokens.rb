a643c98eea7e447e93b3bf29c27d7e7682374fca
Phil G. Sphicas <pgs2111@columbia.edu>
2013-04-30 22:52:29 -0700

    bad regex .. /a-z/ != /[a-z]/ (silly)

--- lib/stack/find_twitter_tokens.rb
+++ lib/stack/find_twitter_tokens.rb
@@ -1,5 +1,5 @@
 class Stack::FindTwitterTokens < Stack::BaseTokenFinder
   tokens :twitter, :consumer_secret  => '[="]([0-9a-zA-Z]{35,44})[&"]',
-                   :consumer_key     => { :matcher => '[="]([0-9a-zA-Z]{18,25})[&"]', :must_have => /a-z/ },
+                   :consumer_key     => { :matcher => '[="]([0-9a-zA-Z]{18,25})[&"]', :must_have => /[a-z]/ },
                    :random_threshold => 0.6
 end
