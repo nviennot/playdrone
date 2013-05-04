011bfbb0edfd81cfe11d0633d69aaedb25007c0a
Nicolas Viennot <nicolas@viennot.biz>
2013-05-04 04:18:33 -0400

    Graphing hashes

--- graphs/resource_blacklist.plot
+++ graphs/resource_blacklist.plot
@@ -1,8 +1,8 @@
 set terminal pdf dashed size 12,3
 set output "resource_blacklist.pdf"
 
-set title "Resource Blacklist"
-set key invert reverse Left outside
+set title "Resource Blacklist (log scale)"
+set key reverse Left outside
 
 set ylabel "Number of Resources Blacklisted" offset +1, 0
 set xlabel "Blacklist Cutoff" offset 0,+0.2
@@ -11,4 +11,5 @@ set grid
 set logscale xy
 set grid xtics ytics
 
-plot 'resource_blacklist.dat' using 1:2 lt 1 lc 0 lw 3 with lines notitle
+plot 'resource_blacklist.dat' using 1:2 lt 1 lc 1 lw 3 with lines t "Resource Names", \
+                           '' using 1:3 lt 1 lc 3 lw 3 with lines t "Asset Hashes"
--- graphs/resource_blacklist.rb
+++ graphs/resource_blacklist.rb
@@ -1,27 +1,49 @@
 #!../script/rails runner
 
-cache_file = Rails.root.join('graphs/resource_blacklist.cache')
+cache_file_resources = Rails.root.join('graphs/resource_blacklist_resources.cache')
+cache_file_asset_hashes = Rails.root.join('graphs/resource_blacklist_asset_hashes.cache')
 
-if cache_file.exist?
-  result = MultiJson.load(File.open(cache_file))
+if cache_file_resources.exist?
+  result_resources = MultiJson.load(File.open(cache_file_resources))
 else
-  result = App.index('2013-05-01').search(
+  result_resources = App.index('2013-05-01').search(
     :size => 0,
     :query => {:match_all => {}},
     :filter => {:term => {:decompiled => true}},
     :facets => {
       :sig => {
-        :terms => { :field => :sig_resources, :size => 2_000_000 }
+        :terms => { :field => :sig_resources, :size => 1_000_000 }
       }
     }
   ).facets['sig'].terms
 
-  File.open(cache_file, 'w') do |f|
-    f.puts MultiJson.dump(result)
+  File.open(cache_file_resources, 'w') do |f|
+    f.puts MultiJson.dump(result_resources)
   end
 end
 
-counts = result.map { |f| f['count'] }
+if cache_file_asset_hashes.exist?
+  result_asset_hashes = MultiJson.load(File.open(cache_file_asset_hashes))
+else
+  result_asset_hashes = App.index('2013-05-01').search(
+    :size => 0,
+    :query => {:match_all => {}},
+    :filter => {:term => {:decompiled => true}},
+    :facets => {
+      :sig => {
+        :terms => { :field => :sig_asset_hashes, :size => 800_000 }
+      }
+    }
+  ).facets['sig'].terms
+
+  File.open(cache_file_asset_hashes, 'w') do |f|
+    f.puts MultiJson.dump(result_asset_hashes)
+  end
+end
+
+
+counts_resources = result_resources.map { |f| f['count'] }
+counts_asset_hashes = result_asset_hashes.map { |f| f['count'] }
 
 buckets = [
   9.times.map { |i| (i+1)*10 },
@@ -31,7 +53,9 @@ buckets = [
 ].flatten
 
 data = buckets.map do |bucket|
-  ["#{bucket}", counts.select { |c| c > bucket }.count]
+  count_resources = counts_resources.select { |c| c > bucket }.count
+  count_asset_hashes = counts_asset_hashes.select { |c| c > bucket }.count
+  [bucket, count_resources, count_asset_hashes.zero? ? nil : count_asset_hashes]
 end
 
 File.open(ARGV[0], 'w') do |f|
