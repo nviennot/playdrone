378fc5b84188ece07bb79389968686f64451369d
Phil G. Sphicas <pgs2111@columbia.edu>
2013-05-05 14:16:16 -0700

    bug fix for token regexps. meta-chars lose their meaning inside brackets, don't need \

--- lib/stack/find_amazon_tokens.rb
+++ lib/stack/find_amazon_tokens.rb
@@ -1,5 +1,5 @@
 class Stack::FindAmazonTokens < Stack::BaseTokenFinder
   tokens :amazon, :access_key_id     => '[="](AKIA[0-9A-Z]{16})[&"]',
-                  :secret_access_key => '[="]([0-9a-zA-Z\/\+]{40})[&"]',
+                  :secret_access_key => '[="]([0-9a-zA-Z/+]{40})[&"]',
                   :proximity         => 5
 end
