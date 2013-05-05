5de90cde34db5c139b6cb983ec9891394c8ae35a
Phil G. Sphicas <pgs2111@columbia.edu>
2013-05-05 05:41:22 -0700

    token stats

---
+++ ideas/token_stats.rb
@@ -0,0 +1,177 @@
+TOKEN_TYPES = [
+  :amazon,
+  :bitlyv1,
+  :bitlyv2,
+  :facebook, 
+  :flickr,
+  :foursquare,
+  :google_maps,
+  :google_oauth2,
+  :linkedin,
+  :twitter,
+  :yelpv1,
+  :yelpv2
+]
+
+MAX_TO_TEST = 25
+# get all the valid tokens
+def get_all_valid_tokens(opts={})
+  opts = {
+    :max_to_test   => MAX_TO_TEST,
+    :include_raw   => true
+  }.merge(opts)
+  TOKEN_TYPES.map { |type| { type => get_valid_tokens(type, opts) } }
+end
+
+# get all valid tokens of a certain type
+def get_valid_tokens(type, opts={})
+  candidates = token_tuples(type)
+  valid_tokens(type, candidates, opts)
+end
+
+# given a list of candidates of a certain type
+# test the credentials and return a bunch of data
+def valid_tokens(type, candidates, opts={})
+  opts = {
+    :include_raw => false
+  }.merge(opts)
+
+  results = {
+    :stats => {},
+  }
+  _candidates = candidates
+  Rails.logger.info "Considering #{candidates.count} #{type} token tuples"
+  results[:stats][:total_candidates] = candidates.count
+  candidates = consolidate_by_frequency candidates
+  Rails.logger.info "Consolidating to #{candidates.count} #{type} unique token tuples"
+  results[:stats][:unique_candidates] = candidates.count
+  num_to_test = opts[:max_to_test] && candidates.count > opts[:max_to_test] ? opts[:max_to_test] : candidates.count
+  Rails.logger.info "Testing #{num_to_test} #{type} token tuples"
+  results[:stats][:unique_tested] = num_to_test
+  raw_results = candidates.each_with_index.map do |tuple, index|
+    { tuple => index < num_to_test ? valid_token_tuple?(type, tuple) : nil}
+  end
+  results[:stats][:unique_valid] = raw_results.select { |x| x.values.first }.count
+  results[:stats][:unique_invalid] = raw_results.select { |x| x.values.first == false }.count
+  results[:stats][:unique_untested] = raw_results.select { |x| x.values.first == nil }.count
+  result_lookup = raw_results.reduce({}) { |x,y| x.merge(y) }
+  total_results =_candidates.map do |tuple|
+    { tuple => result_lookup[tuple] }
+  end
+  results[:stats][:unique_percent_valid] = results[:stats][:unique_valid].to_f / results[:stats][:unique_tested]
+  results[:stats][:unique_coverage] = 1.0 - results[:stats][:unique_untested].to_f / results[:stats][:unique_candidates]
+
+  results[:stats][:total_valid] = total_results.select { |x| x.values.first }.count
+  results[:stats][:total_invalid] = total_results.select { |x| x.values.first == false }.count
+  results[:stats][:total_untested] = total_results.select { |x| x.values.first == nil }.count
+  results[:stats][:total_tested] = results[:stats][:total_candidates] - results[:stats][:total_untested]
+
+  results[:stats][:total_percent_valid] = results[:stats][:total_valid].to_f / results[:stats][:total_tested]
+  results[:stats][:total_coverage] = 1.0 - results[:stats][:total_untested].to_f / results[:stats][:total_candidates]
+
+  if opts[:include_raw]
+    results[:raw] = {}
+    results[:raw][:tested_results] = raw_results
+    results[:raw][:total_results] = total_results
+  end
+  results
+end
+
+# given an array with non-unique elements,
+# consolidate into a unique array, with the
+# most frequently occurring values first
+def consolidate_by_frequency(array)
+  array.group_by{|x| x}.values.sort_by{|group| group.count}.reverse.flatten(1).uniq
+end
+
+# check if a token tuple is valid
+def valid_token_tuple?(type, *args)
+  args.flatten!
+  case type
+    when :amazon        then valid_amazon_tokens?(*args)
+    when :bitlyv1       then valid_bitlyv1_tokens?(args[1], args[0])
+    when :bitlyv2       then valid_bitlyv2_tokens?(*args)
+    when :facebook      then valid_facebook_tokens?(args[1], args[0])
+    when :flickr        then valid_flickr_tokens?(*args)
+    when :foursquare    then valid_foursquare_tokens?(*args)
+    when :google_maps   then valid_google_maps_tokens?(*args)
+    when :google_oauth2 then valid_google_oauth2_tokens?(*args)
+    when :linkedin      then valid_linkedin_tokens?(args[1], args[0])
+    when :twitter       then valid_twitter_tokens?(args[1], args[0])
+    when :yelpv1        then valid_yelpv1_tokens?(*args)
+    when :yelpv2        then valid_yelpv2_tokens?(*args)
+  else
+    Rails.logger.error "Unknown token type: #{type}"
+  end
+end
+
+# total number of tokens (of a given type)
+# found across all apps.
+# if a single app has multiple (distinct)
+# tokens of the same type, each token
+# counts independently
+def count_tokens(type)
+  token_tuples(type).count
+end
+
+# number of unique tokens (of a given type)
+def count_unique_tokens(type)
+  token_tuples(type).uniq.count
+end
+
+# number of apps with at least
+# one token of the specified type
+def count_apps_using_tokens(type)
+  get_tokens(type).count
+end
+
+# ES query for the tokens, per app
+def get_tokens(type)
+  fields = token_fields(type)
+  tokenclass = token_class(type)
+  count = "#{tokenclass.token_name}_count"
+  App.index('_all').search(:size   => 100000,
+                           :query  => { :match_all => {} },
+                           :filter => { :script => { :script => "doc['#{count}'].value > 0" } },
+                           :fields => fields,
+                           :facets => {tokenclass.token_name => {:statistical => { :field => "#{count}" } } } ).results
+end
+
+# ES gives us this:
+# :key    => [key1, key2, ...]
+# :secret => [secret1, secret2, ...]
+# We really want this:
+# [[key1, secret1]
+#  [key2, secret2]]
+#  ..
+def token_tuples(type)
+  results = get_tokens(type)
+  results.map do |app_result|
+    token_fields(type).map do |field|
+      app_result[field]
+    end.transpose
+  end.flatten(1)
+end
+
+def token_class(type)
+  eval "Stack::Find#{type.to_s.camelize}Tokens"
+end
+
+def token_fields(type)
+  tokenclass = token_class(type)
+  fields = tokenclass.token_filters.keys.map { |key| "#{tokenclass.token_name}_#{key}" }
+end
+
+def test_credentials(type, *creds)
+  eval 'valid_#{type}_tokens? #{creds.join(',')}'
+end
+
+def count_all
+  TOKEN_TYPES.each do |type|
+    puts "#{type}: #{(get_tokens type).count}"
+  end
+end
+
+def all_app_ids
+  app_ids = App.index('_all').search(:size => 10000000, :query => {:match_all => {}}, :fields => [:_id]).results.map(&:_id).uniq
+end
