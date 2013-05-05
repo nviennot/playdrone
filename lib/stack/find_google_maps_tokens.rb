4ae6778da71afbb3426e3b80cc93d32a9399d123
Phil G. Sphicas <pgs2111@columbia.edu>
2013-05-05 14:16:16 -0700

    bug fix for token regexps. meta-chars lose their meaning inside brackets, don't need \

--- lib/stack/find_google_maps_tokens.rb
+++ lib/stack/find_google_maps_tokens.rb
@@ -1,4 +1,4 @@
 class Stack::FindGoogleMapsTokens < Stack::BaseTokenFinder
-  tokens :google_maps, :api_key => '(?:maps|MAPS|place|PLACE|key|KEY).*?[="](AI[0-9a-zA-Z_\-]{37})[&"]',
+  tokens :google_maps, :api_key => '(?:maps|MAPS|place|PLACE|key|KEY).*?[="](AI[0-9a-zA-Z_-]{37})[&"]',
                        :random_threshold => 0.3
 end
--- lib/stack/find_yelpv1_tokens.rb
+++ lib/stack/find_yelpv1_tokens.rb
@@ -1,4 +1,4 @@
 class Stack::FindYelpv1Tokens < Stack::BaseTokenFinder
-  tokens :yelpv1, :ywsid            => '(?:ywsid|YWSID).*?[="]([0-9a-zA-Z_\-]{22})["&]',
+  tokens :yelpv1, :ywsid            => '(?:ywsid|YWSID).*?[="]([0-9a-zA-Z_-]{22})["&]',
                   :random_threshold => 0.2
 end
