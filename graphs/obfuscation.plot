41fbd2bab3b58640ac513a74880179decf906f91
Nicolas Viennot <nicolas@viennot.biz>
2013-07-27 22:20:45 -0400

    Obfuscation graph (market wide -> market wide, new, updated)

--- graphs/obfuscation.plot
+++ graphs/obfuscation.plot
@@ -3,7 +3,7 @@ set output "obfuscation.pdf"
 
 #set title "App categories"
 set key horizontal
-set key bottom center outside
+set key top center
 # set nokey
 
 set ylabel "Obfuscation rate (%)" offset +1, 0
@@ -21,7 +21,7 @@ set xtics nomirror rotate by -20
 #set yrange [-5000:6000]
 
 set xtics 604800
-set ytics 0.1
+set ytics 5
 
 
 #set style fill solid border -1
@@ -31,4 +31,6 @@ set ytics 0.1
 
 set xzeroaxis lw 5 lt -1
 
-plot 'obfuscation.dat' using 1:2 notitle with lines
+plot 'obfuscation.dat' using 1:2 title "All Market"   lt 1 lc 1 with lines, \
+                    '' using 1:3 title "New Apps"     lt 1 lc 2 with lines, \
+                    '' using 1:4 title "Updated Apps" lt 1 lc 3 with lines
--- graphs/obfuscation.rb
+++ graphs/obfuscation.rb
@@ -1,49 +1,65 @@
 #!../script/rails runner
 
-results = {}
-result = App.index('*').search(
-  :size => 0,
-  :query => { :term => { :decompiled => true } },
-  :facets => {
-    :obfuscated => {
-      :date_histogram => {
-        :key_field => :crawled_at,
-        :value_script => "doc['obfuscated'].value == 'T' ? 1 : 0",
-        :interval => :day
-      }
-    },
-    :all => {
-      :date_histogram => {
-        :key_field => :crawled_at,
-        :value_script => "1",
-        :interval => :day
-      }
-    },
-  }
-)
+def get_result(filter=nil)
+  App.index('*').search(
+    :size => 0,
+    :query => { :term => { :decompiled => true } },
 
-data = result.facets.obfuscated['entries'].size.times.map do |i|
-  all = result.facets.all['entries'][i]
-  obfuscated = result.facets.obfuscated['entries'][i]
+    :query => { :bool => { :must => (
+      [{ :term =>  { :decompiled => true } }] +
+      [filter]
+    ).compact } },
 
-  day = obfuscated['time'] / 1000
-  if Time.at(day).to_date <= Date.parse("2013-04-26")
-  elsif Time.at(day).to_date == Date.parse("2013-05-04")
-  elsif Time.at(day).to_date == Date.parse("2013-05-05")
-  elsif Time.at(day).to_date == Date.parse("2013-05-06")
-  elsif Time.at(day).to_date == Date.parse("2013-06-03")
-  elsif Time.at(day).to_date == Date.parse("2013-06-04")
-  elsif Time.at(day).to_date == Date.parse("2013-06-05")
-    # no good data
-    nil
-  else
-    results[day] = { :all => all['total'].to_i, :obfuscated => obfuscated['total'].to_i }
-  end
+
+    :facets => {
+      :obfuscated => {
+        :date_histogram => {
+          :key_field => :crawled_at,
+          :value_script => "doc['obfuscated'].value == 'T' ? 1 : 0",
+          :interval => :day
+        }
+      },
+      :all => {
+        :date_histogram => {
+          :key_field => :crawled_at,
+          :value_script => "1",
+          :interval => :day
+        }
+      },
+    }
+  )
 end
 
+def get_timehash(result, facet)
+  Hash[result.facets[facet]['entries'].map { |e| [Time.at(e['time']/1000).to_date, e['total'].to_i] }]
+end
+
+def get_rate(result)
+  obfuscated = get_timehash(result, :obfuscated)
+  Hash[get_timehash(result, :all).map do |day, value|
+    [day, 100*obfuscated[day].to_f / value.to_f]
+  end]
+end
+
+all_rates = get_rate(get_result(nil))
+new_rates = get_rate(get_result(:term => {:market_released => true}))
+updated_rates = get_rate(get_result(:term => {:apk_updated => true}))
+
+results = {}
+
 File.open(ARGV[0], 'w') do |f|
-  last_values = {}
-  results.each do |day, value|
-    f.puts [day, 100*value[:obfuscated]/value[:all].to_f].compact.join(' ')
+  all_rates.keys.each do |day|
+    if    day <= Date.parse("2013-04-26")
+    elsif day == Date.parse("2013-05-04")
+    elsif day == Date.parse("2013-05-05")
+    elsif day == Date.parse("2013-05-06")
+    elsif day == Date.parse("2013-06-03")
+    elsif day == Date.parse("2013-06-04")
+    elsif day == Date.parse("2013-06-05")
+      # no good data
+      nil
+    else
+      f.puts [day.to_time.to_i, all_rates[day], new_rates[day], updated_rates[day]].compact.join(' ')
+    end
   end
 end
