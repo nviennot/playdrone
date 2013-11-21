20a1685b6b804a17b5a28e014a993829102c9eb6
Nicolas Viennot <nicolas@viennot.biz>
2013-11-21 01:53:31 -0500

    Fix encoding when reading from dictionnary

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 every :day, :at => '2:00am', :roles => [:master] do
   runner "ES.create_all_indexes and ProcessApp.process_all(Date.today)"
-  runner "File.open('/root/dic').each { |l| SearchApp.perform_async(l.chomp) }"
+  runner "File.open('/root/dic').each { |l| SearchApp.perform_async(l.unpack('C*').pack('U*').chomp) }"
 end
 
 every :day, :at => '2:00am' do
