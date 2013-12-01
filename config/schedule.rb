9f8324cc2396d351270f3f825337ea36ae459228
Nicolas Viennot <nicolas@viennot.biz>
2013-12-01 00:53:41 -0500

    schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,12 @@
-every :day, :at => '2:15am', :roles => [:master] do
+every :day, :at => '1:00am', :roles => [:master] do
   runner "ES.create_all_indexes and ProcessApp.process_all(Date.today)"
 end
 
-every :day, :at => '9:00am', :roles => [:master] do
+every :day, :at => '2:00am', :roles => [:master] do
   runner "File.open('/root/dic').to_a.shuffle.each { |l| SearchApp.perform_async(l.unpack('C*').pack('U*').chomp) }"
 end
 
-every :day, :at => '2:20am' do
+every :day, :at => '1:00am' do
   command "service sidekiq-market restart"
 
   2.times do |i|
