1effd50d39d8eadc15fbe2ad0e15622e474c5986
Nicolas Viennot <nicolas@viennot.biz>
2014-08-06 13:30:12 -0400

    capistrano settings

--- deploy/settings.rb
+++ deploy/settings.rb
@@ -1,4 +1,4 @@
-set :repository, 'git@github.com:nviennot/google-play-crawler.git'
+set :repository, 'git@github.com:nviennot/playdrone.git'
 set :scm, :git
 set :branch, 'master'
 set :deploy_via, :remote_cache
@@ -8,8 +8,8 @@ set :user, 'root'
 set :use_sudo, false
 ssh_options[:forward_agent] = true
 
-set :application, 'google-play-crawler'
-set :rvm_ruby_string, 'ruby-2.0@google-play-crawler'
+set :application, 'playdrone'
+set :rvm_ruby_string, 'ruby-2.1'
 set :deploy_to, "/srv/#{application}"
 set :keep_releases, 3
 set :normalize_asset_timestamps, false
@@ -19,30 +19,7 @@ set :rvm_type, :system
 set :whenever_roles, [:sidekiq, :master]
 set :whenever_command, 'bundle exec whenever'
 
-if ENV['VAGRANT']
-  role :unicorn, '10.1.1.11'
-  role :sidekiq, '10.1.1.11', '10.1.1.12'
-  role :metrics, '10.1.1.12'
-  role :master,  '10.1.1.11'
-else
-  role :unicorn, 'node01.playdrone.io', 'node02.playdrone.io',
-                 'node03.playdrone.io', 'node04.playdrone.io',
-                 'node05.playdrone.io', 'node06.playdrone.io',
-                 'node07.playdrone.io', 'node08.playdrone.io',
-                 'node09.playdrone.io', 'node10.playdrone.io',
-                 'node11.playdrone.io', 'node12.playdrone.io',
-                 'node13.playdrone.io', 'node14.playdrone.io',
-                 'node15.playdrone.io', 'node16.playdrone.io'
-
-  role :sidekiq, 'node01.playdrone.io', 'node02.playdrone.io',
-                 'node03.playdrone.io', 'node04.playdrone.io',
-                 'node05.playdrone.io', 'node06.playdrone.io',
-                 'node07.playdrone.io', 'node08.playdrone.io',
-                 'node09.playdrone.io', 'node10.playdrone.io',
-                 'node11.playdrone.io', 'node12.playdrone.io',
-                 'node13.playdrone.io', 'node14.playdrone.io',
-                 'node15.playdrone.io', 'node16.playdrone.io'
-
-  role :metrics, 'node01.playdrone.io'
-  role :master,  'node01.playdrone.io'
-end
+role :unicorn, 'playdrone.viennot.com'
+role :sidekiq, 'playdrone.viennot.com'
+role :metrics, 'playdrone.viennot.com'
+role :master,  'playdrone.viennot.com'
