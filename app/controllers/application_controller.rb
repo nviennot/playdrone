15d4d39e05ac7bf9b611206b8498582f3063f830
Nicolas Viennot <nicolas@viennot.biz>
2014-09-30 01:03:31 -0400

    Fix 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from(Stretcher::RequestError::NotFound) { head :not_found }
+  rescue_from Stretcher::RequestError::NotFound, :with => :render_404
+
+  private
+
+  def render_404
+    render :file => "#{Rails.root}/public/404", :formats => [:html], :status => 404, :layout => false
+  end
 end
