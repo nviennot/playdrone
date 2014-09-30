fe1e51bf5b4b849571ef1d363fb8a9f912f31fa2
Nicolas Viennot <nicolas@viennot.biz>
2014-09-30 16:34:46 -0400

    objdump .so files

--- lib/stack/index_sources.rb
+++ lib/stack/index_sources.rb
@@ -1,7 +1,7 @@
 class Stack::IndexSources < Stack::Base
   def call(env)
     if env[:src_dir] || env[:force_index_sources]
-      filter = /\.(java|xml|html|js)$/
+      filter = /\.(java|xml|html|js|so)$/
       env[:need_src].call(:include_filter => filter)
 
       canonical_path = "src/#{env[:app_id].gsub(/\./, '/')}"
@@ -23,6 +23,28 @@ class Stack::IndexSources < Stack::Base
             :lines     => File.read(fullpath).lines.map(&:chomp) }
         end.compact
       end
+
+      so_files_seen = Set.new
+      sources += Dir["#{env[:src_dir]}/lib/armeabi*/**/*.so"].map do |fullpath|
+        next unless File.file?(fullpath)
+        path = fullpath.split("#{env[:src_dir]}/").last
+        filename = path.split('/').last
+        next if so_files_seen.include?(filename)
+        so_files_seen << filename
+
+        args = ["objdump", '-t', '-T', fullpath]
+        output = Stack::Base.new(nil).exec_and_capture(*args)
+        raise "Can't objdump on #{path}" unless $?.success?
+
+        { :_type     => 'source',
+          :app_id    => env[:app_id],
+          :canonical => false,
+          :path      => path,
+          :filename  => path.split('/').last,
+          :extention => 'so',
+          :crawed_at => env[:crawled_at],
+          :lines     => output.split("\n") }
+      end.compact
 
       StatsD.measure 'stack.index_sources' do
         # Source.index(:src).delete_query(:query => {:term => {:app_id => env[:app_id] }}) rescue nil
