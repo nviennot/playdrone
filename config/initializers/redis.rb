286f5114985d6c1debdb71868c477d5270fcd49f
Nicolas Viennot <nicolas@viennot.biz>
2014-10-19 13:20:57 -0400

    fix attempt

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,17 +1,22 @@
 ENV['REDIS_URL'] ||= YAML.load_file(Rails.root.join("config", "redis.yml"))[Rails.env]['url']
 
+# def Redis.instance
+  # # XXX Not tolerant to forks
+  # return @_redis_instance if @_redis_instance
+
+  # # redis = Redis.new(:driver => :hiredis)
+  # redis = Redis.new(:timeout => 10*60)
+  # version = redis.info['redis_version']
+  # unless Gem::Version.new(version) >= Gem::Version.new('2.6.0')
+    # raise "You are using Redis #{version}. Please use Redis 2.6.0 or later."
+  # end
+
+  # @_redis_instance = redis
+# end
+
+# Let's try per threads connections...
 def Redis.instance
-  # XXX Not tolerant to forks
-  return @_redis_instance if @_redis_instance
-
-  # redis = Redis.new(:driver => :hiredis)
-  redis = Redis.new(:timeout => 10*60)
-  version = redis.info['redis_version']
-  unless Gem::Version.new(version) >= Gem::Version.new('2.6.0')
-    raise "You are using Redis #{version}. Please use Redis 2.6.0 or later."
-  end
-
-  @_redis_instance = redis
+  Thread.current[:redis_instance] ||= Redis.new(:timeout => 10*60)
 end
 
 def Redis.for_apps
